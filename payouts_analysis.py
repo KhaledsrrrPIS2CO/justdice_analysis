93248a48e3a3119a4e3b4c494cd0422edbbbabc8
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-04-11 19:20:17 +0200

    Finishing payouts_analysis.py

--- payouts_analysis.py
+++ payouts_analysis.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib.dates as mdates
 import numpy as np
 import seaborn as sns
 
 
 def load_data(file_path):
     return pd.read_csv(file_path)
+
+
+def convert_unix_time_to_datetime(file_path):
+    # Read the CSV file into a dataframe
+    df = pd.read_csv(file_path)
+
+    # Convert the Unix timestamps to Datetime objects
+    for col in df.columns:
+        if "event_date" in col:
+            df[col] = pd.to_datetime(df[col]).dt.date
+
+    return df
 
 
 def calculate_total_average_max_and_min_payouts(payouts):
@@ -29,16 +41,6 @@ def calculate_average_payouts_per_group(merged_data, group_column):
     return merged_data.groupby(group_column)["value_usd"].mean().reset_index()
 
 
-def plot_daily_payouts(daily_payouts):
-    sns.set(style="whitegrid")
-    plt.figure(figsize=(15, 6))
-    sns.lineplot(data=daily_payouts, x="event_date", y="value_usd")
-    plt.title("Daily Payouts")
-    plt.xlabel("Event Date")
-    plt.ylabel("Payouts (USD)")
-    plt.show()
-
-
 def get_temporal_scope(dataframe, date_column):
     min_date = dataframe[date_column].min()
     max_date = dataframe[date_column].max()
@@ -51,100 +53,262 @@ def count_install_ids(dataframe, column):
     return unique_install_ids, total_install_ids
 
 
-def plot_moving_average(daily_payouts, window_size):
+def calculate_central_tendency(dataframe, column):
+    mean = dataframe[column].mean()
+    median = dataframe[column].median()
+    mode = dataframe[column].mode().values.tolist()[0]
+    return mean, median, mode
+
+
+def get_payouts_info(dataframe):
+    shape = dataframe.shape
+    num_all_install_ids = dataframe["install_id"].count()
+    num_unique_install_ids = dataframe["install_id"].nunique()
+    return shape, num_all_install_ids, num_unique_install_ids
+
+
+def read_and_explore(file_path):
+    payouts = file_path
+    print("\nExplore payouts DF:", payouts, "\n")
+    return payouts
+
+
+def plot_calculate_time_series(daily_payouts, window_size):
     daily_payouts["moving_average"] = daily_payouts["value_usd"].rolling(window=window_size).mean()
 
     plt.figure(figsize=(15, 6))
     sns.lineplot(data=daily_payouts, x="event_date", y="value_usd", label="Daily Payouts")
     sns.lineplot(data=daily_payouts, x="event_date", y="moving_average", label=f"{window_size}-Day Moving Average")
-    plt.title("Daily Payouts with Moving Average")
+    plt.title("Daily Payouts over time with Moving Average")
+    plt.tight_layout()
     plt.xlabel("Event Date")
     plt.ylabel("Payouts (USD)")
-    plt.legend()
-    plt.savefig("Time_series_plot_for_payouts_over_time_with_Moving_Average.png")
+
+    # Update the x-axis to display month names
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+    plt.xticks(rotation=45)  # Optional: Rotate the x-axis labels for better readability
+    plt.tight_layout()
+    plt.savefig('PTime_series_plot_for_payouts_over_time_with_Moving_Average.png', dpi=300, bbox_inches='tight')
     plt.show()
 
 
-def plot_install_payout_deciles(dataframe, column):
-    # Calculate the deciles of the payouts column
-    deciles = dataframe[column].quantile([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]).tolist()
+def calculate_plot_payouts_by_decile_percentage(dataframe):
+    # Calculate the payouts for each install ID
+    payouts_by_id = dataframe.groupby("install_id")["value_usd"].sum().reset_index()
 
-    # Group the dataframe by install ID and calculate the total payout for each install ID
-    payouts_by_install = dataframe.groupby("install_id")[column].sum().reset_index()
+    # Divide the install IDs into deciles based on their payouts
+    payouts_by_id["decile"] = pd.qcut(payouts_by_id["value_usd"], 10, labels=False)
 
-    # Categorize the payouts by decile
-    payouts_by_install["decile"] = pd.cut(payouts_by_install[column], bins=[-np.inf] + deciles + [np.inf],
-                                          labels=["<10%", "10-20%", "20-30%", "30-40%", "40-50%", "50-60%", "60-70%",
-                                                  "70-80%", "80-90%", ">90%"])
+    # Calculate the total payouts for each decile
+    decile_payouts = payouts_by_id.groupby("decile")["value_usd"].sum().reset_index()
 
-    # Calculate the number of installs in each decile
-    install_counts = payouts_by_install["decile"].value_counts().sort_index()
+    # Calculate the payout contribution for each decile
+    total_payouts = decile_payouts["value_usd"].sum()
+    decile_payouts["payout_contribution"] = decile_payouts["value_usd"] / total_payouts * 100
 
-    # Plot the bar graph
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x=install_counts.index, y=install_counts.values)
-    plt.title("Install ID Payout Contribution by Deciles")
-    plt.xlabel("Decile")
-    plt.ylabel("Number of Installs")
+    # Reorder the deciles by payout contribution and reset the index
+    decile_payouts = decile_payouts.sort_values("payout_contribution", ascending=False).reset_index(drop=True)
+
+    # Generate a bar graph of the payout contribution for each decile
+    x_labels = ["Decile 1", "Decile 2", "Decile 3", "Decile 4", "Decile 5",
+                "Decile 6", "Decile 7", "Decile 8", "Decile 9", "Decile 10"]
+    ax = plt.gca()
+    ax.bar(x_labels, decile_payouts["payout_contribution"], color="blue")
+    plt.title("Payouts by Decile (%)")
+    plt.ylabel("Percentage of total payouts")
+    plt.xticks(rotation=45)
+
+    # Add the percentage values on top of each bar
+    for i, val in enumerate(decile_payouts["payout_contribution"]):
+        ax.text(i, val + 1, f"{val:.1f}%", ha="center")
+    plt.tight_layout()
+    plt.savefig('Payouts by Decile percentage', dpi=300, bbox_inches='tight')
     plt.show()
 
+    return decile_payouts[["decile", "payout_contribution", "value_usd"]]
 
-def plot_install_payout_deciles(dataframe, column):
-    # Calculate the deciles of the payouts column
-    deciles = dataframe[column].quantile([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]).tolist()
 
-    # Group the dataframe by install ID and calculate the total payout for each install ID
-    payouts_by_install = dataframe.groupby("install_id")[column].sum().reset_index()
+def calculate_plot_payouts_by_decile_usd(dataframe):
+    # Calculate the payouts for each install ID
+    payouts_by_id = dataframe.groupby("install_id")["value_usd"].sum().reset_index()
 
-    # Categorize the payouts by decile
-    payouts_by_install["decile"] = pd.cut(payouts_by_install[column], bins=[-np.inf] + deciles + [np.inf], labels=["<10%", "10-20%", "20-30%", "30-40%", "40-50%", "50-60%", "60-70%", "70-80%", "80-90%", ">90%"])
+    # Divide the install IDs into deciles based on their payouts
+    payouts_by_id["decile"] = pd.qcut(payouts_by_id["value_usd"], 10, labels=False)
 
-    # Calculate the USD value of payouts in each decile
-    payouts_by_decile = payouts_by_install.groupby("decile")[column].sum().reset_index()
+    # Calculate the total payouts for each decile
+    decile_payouts = payouts_by_id.groupby("decile")["value_usd"].sum().reset_index()
 
-    # Plot the bar graph
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x="decile", y=column, data=payouts_by_decile)
-    plt.title("Install ID Payout Contribution by Deciles")
-    plt.xlabel("Decile")
+    # Reorder the deciles and reset the index
+    decile_payouts = decile_payouts.sort_values("value_usd", ascending=False).reset_index(drop=True)
+
+    # Generate a bar graph of the payouts for each decile
+    x_labels = ["Decile 1", "Decile 2", "Decile 3", "Decile 4", "Decile 5",
+                "Decile 6", "Decile 7", "Decile 8", "Decile 9", "Decile 10"]
+    plt.bar(x_labels, decile_payouts["value_usd"], color="blue")
+    plt.title("Payouts by Decile (USD)")
     plt.ylabel("Payouts (USD)")
+    plt.xticks(rotation=45)
+
+    # Add USD value on top of each bar
+    for i, value in enumerate(decile_payouts["value_usd"]):
+        plt.text(i, value, f"${value:.0f}", ha='center')
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('Payouts by Decile usd', dpi=300, bbox_inches='tight')
+
+    return decile_payouts[["decile", "value_usd"]]
+
+
+
+
+def plot_and_calculate_biggest_decile_to_ten_deciles_usd(dataframe):
+    # Calculate the payouts for each install ID
+    payouts_by_id = dataframe.groupby("install_id")["value_usd"].sum().reset_index()
+
+    # Divide the install IDs into 10 deciles based on their payouts
+    payouts_by_id["decile"] = pd.qcut(payouts_by_id["value_usd"], 10, labels=False)
+
+    # Calculate the total payouts for the top decile
+    top_decile_payouts = payouts_by_id[payouts_by_id["decile"] == 9]["value_usd"].sum()
+
+    # Divide the top decile into 10 smaller deciles
+    top_decile = payouts_by_id[payouts_by_id["decile"] == 9].sort_values("value_usd", ascending=False)
+    top_decile["sub_decile"] = pd.qcut(top_decile["value_usd"], 10, labels=False)
+
+    # Calculate the total payouts for each sub-decile
+    sub_decile_payouts = top_decile.groupby("sub_decile")["value_usd"].sum().reset_index()
+
+    # Sort the sub-deciles in descending order of value
+    sub_decile_payouts = sub_decile_payouts.sort_values("value_usd", ascending=False)
+
+    # Generate a bar graph of the payouts for each sub-decile
+    x_labels = [f" Decile {i + 1}" for i in range(10)]
+    plt.bar(x_labels, sub_decile_payouts["value_usd"], color="blue")
+
+    # Add labels for sub-decile payouts
+    for i, value in enumerate(sub_decile_payouts["value_usd"]):
+        plt.text(i, value, f"${value:.0f}", ha='center', va='bottom')
+
+    plt.title("Payouts by Decile of top 10% of payouts (USD)")
+    plt.xlabel("")
+    plt.ylabel("Payouts (USD)")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('Payouts by Decile of top 10 of payouts (USD).png', dpi=300, bbox_inches='tight')
+
+    return sub_decile_payouts[["sub_decile", "value_usd"]]
+
+def plot_and_calculate_biggest_decile_to_ten_deciles_percent(dataframe):
+    total_payouts = dataframe["value_usd"].sum()
+    # Calculate the payouts for each install ID
+    payouts_by_id = dataframe.groupby("install_id")["value_usd"].sum().reset_index()
+
+    # Divide the install IDs into 10 deciles based on their payouts
+    payouts_by_id["decile"] = pd.qcut(payouts_by_id["value_usd"], 10, labels=False)
+
+    # Calculate the total payouts for the top decile
+    top_decile_payouts = payouts_by_id[payouts_by_id["decile"] == 9]["value_usd"].sum()
+
+    # Divide the top decile into 10 smaller deciles
+    top_decile = payouts_by_id[payouts_by_id["decile"] == 9].sort_values("value_usd", ascending=False)
+    top_decile["sub_decile"] = pd.qcut(top_decile["value_usd"], 10, labels=False)
+
+    # Calculate the total payouts for each sub-decile
+    sub_decile_payouts = top_decile.groupby("sub_decile")["value_usd"].sum().reset_index()
+
+    # Sort the sub-deciles in descending order of value
+    sub_decile_payouts = sub_decile_payouts.sort_values("value_usd", ascending=False)
+
+    # Calculate the percentage of payouts for each sub-decile
+    sub_decile_payouts["payout_percentage"] = sub_decile_payouts["value_usd"] / total_payouts * 100
+
+    # Generate a bar graph of the payouts for each sub-decile
+    x_labels = [f" Decile {i + 1}" for i in range(10)]
+    plt.bar(x_labels, sub_decile_payouts["payout_percentage"], color="blue")
+
+    # Add labels for sub-decile payouts
+    for i, value in enumerate(sub_decile_payouts["payout_percentage"]):
+        plt.text(i, value, f"{value:.1f}%", ha='center', va='bottom')
+
+    plt.title("Payouts by Decile of top 10% of payouts (USD)")
+    plt.xlabel("")
+    plt.ylabel("Percentage of Payouts (%)")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('Payouts by Decile of top 10 of payouts (percent).png', dpi=300, bbox_inches='tight')
+
+    return sub_decile_payouts[["sub_decile", "value_usd", "payout_percentage"]]
+
+
+####
+
+
+def pareto_distribution(dataframe):
+    # Calculate the total payouts for each install ID
+    payouts_by_id = dataframe.groupby("install_id")["value_usd"].sum().reset_index()
+
+    # Calculate the cumulative percentage of total payouts
+    payouts_by_id = payouts_by_id.sort_values("value_usd", ascending=False).reset_index(drop=True)
+    payouts_by_id["cumulative_percentage"] = payouts_by_id["value_usd"].cumsum() / payouts_by_id[
+        "value_usd"].sum() * 100
+
+    # Plot the Pareto distribution
+    percentiles = np.linspace(0, 100, len(payouts_by_id))
+    selected_percentiles = np.arange(0, 101, 5)
+    selected_cumulative_percentage = np.interp(selected_percentiles, percentiles,
+                                               payouts_by_id["cumulative_percentage"])
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.plot(selected_percentiles, selected_cumulative_percentage, marker='o')
+    ax.set_xlabel('Percentile of install_id')
+    ax.set_ylabel('Cumulative Percentage of Total Payouts')
+    ax.set_title('Pareto Distribution of Total Payouts by install_id')
+    ax.set_xticks(np.arange(0, 101, 10))
+    ax.set_yticks(np.arange(0, 101, 10))
+    plt.grid()
+    plt.savefig('PPareto Distribution of Total Payouts by install_id.png', dpi=300, bbox_inches='tight')
     plt.show()
 
+    return payouts_by_id[["install_id", "value_usd", "cumulative_percentage"]]
 
 
 def main(payouts_file_path, installs_file_path):
-    payouts = load_data(payouts_file_path)
-    installs = load_data(installs_file_path)
+    payouts_path = load_data(payouts_file_path)
+
+    start_date, end_date = get_temporal_scope(payouts_path, "event_date")
+    print(f"\nTemporal scope of payouts_converted.csv: {start_date} - {end_date}")
 
     total_payouts, average_payout_per_install, max_payout, min_payout = calculate_total_average_max_and_min_payouts(
-        payouts)
+        payouts_path)
     print(f"\nTotal Payouts (USD): {total_payouts}\nAverage Payout per Install: {average_payout_per_install}"
           f"\nMaximum Payout: {max_payout}\nMinimum Payout: {min_payout}")
 
-    daily_payouts = calculate_daily_payouts(payouts)
-    plot_daily_payouts(daily_payouts)
+    # Read and explore the data
+    read_and_explore(payouts_path)
 
-    merged_data = merge_installs_and_payouts(installs, payouts)
+    payouts_path = convert_unix_time_to_datetime(payouts_file_path)
 
-    average_payout_per_country = calculate_average_payouts_per_group(merged_data, "country_id")
-    print(f"\nAverage Payout per Country:\n{average_payout_per_country}")
+    shape, num_all_install_ids, num_unique_install_ids = get_payouts_info(payouts_path)
+    print(f"Shape of Data: {shape}\nNumber of All Install IDs: {num_all_install_ids}"
+          f"\nNumber of Unique Install IDs: {num_unique_install_ids}\n")
 
-    average_payout_per_app = calculate_average_payouts_per_group(merged_data, "app_id")
-    print(f"\nAverage Payout per App:\n{average_payout_per_app}")
-
-    average_payout_per_network = calculate_average_payouts_per_group(merged_data, "network_id")
-    print(f"\nAverage Payout per Network:\n{average_payout_per_network}")
-
-    start_date, end_date = get_temporal_scope(payouts, "event_date")
-    print(f"\nTemporal scope of payouts_converted.csv: {start_date} - {end_date}")
-
-    unique_install_ids, total_install_ids = count_install_ids(installs, "install_id")
+    unique_install_ids, total_install_ids = count_install_ids(payouts_path, "install_id")
     print(f"\nUnique Install IDs: {unique_install_ids}\nTotal Install IDs: {total_install_ids}")
 
-    daily_payouts = calculate_daily_payouts(payouts)
-    plot_moving_average(daily_payouts, window_size=7)
+    mean, median, mode = calculate_central_tendency(payouts_path, "value_usd")
+    print(f"Mean: {mean}\nMedian: {median}\nMode: {mode}")
 
-    plot_install_payout_deciles(payouts, "value_usd")
+    daily_payouts = calculate_daily_payouts(payouts_path)
+    plot_calculate_time_series(daily_payouts, window_size=30)
+
+    calculate_plot_payouts_by_decile_percentage(payouts_path)
+    calculate_plot_payouts_by_decile_usd(payouts_path)
+    plot_and_calculate_biggest_decile_to_ten_deciles_usd(payouts_path)
+    plot_and_calculate_biggest_decile_to_ten_deciles_percent(payouts_path)
+    pareto_distribution(payouts_path)
 
 
 if __name__ == "__main__":
